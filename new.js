ed281aa6b04c84ff753c331e4b56fcce4e557b0f
Arshad Shah <ashshah94@yahoo.in>
2022-12-08 07:18:44 +0530

    commit

---
+++ new.js
@@ -0,0 +1,29 @@
+// Init GitHub
+const github = new GitHub();
+// Init UI
+const ui = new UI();
+
+// search input and add event listener
+const searchUser = document
+  .getElementById('searchUser')
+  .addEventListener('keyup', (e) => {
+    // get input text
+    const userText = e.target.value;
+
+    if (userText !== '') {
+      // make http call
+      github.getUser(userText).then(async (data) => {
+        if (data.profile.message === 'Not Found') {
+          // show alert
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          // show profile
+          await ui.showProfile(data.profile);
+          await ui.showRepos(data.repos);
+        }
+      });
+    } else {
+      // clear profile
+      ui.clearProfile();
+    }
+  });
